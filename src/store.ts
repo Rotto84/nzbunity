4e77283597c6dbac7dd26b2d23dfea89b3dab39c
ROtto84 <wwebly@gmail.com>
2026-06-11 23:02:38 -0400

    added amenzb.moe 1 click adapter

--- src/store.ts
+++ src/store.ts
@@ -81,6 +81,7 @@ export const DefaultDownloaderOptions: DownloaderOptions = {
 export const DefaultIndexers: Record<string, IndexerOptions> = {
   abnzb: { Display: 'abNZB', Enabled: true },
   althub: { Display: 'altHUB', Enabled: true },
+  amenzb: { Display: 'ameNZB', Enabled: true },
   animetosho: { Display: 'AnimeTosho', Enabled: true },
   binsearch: { Display: 'BinSearch', Enabled: true },
   dognzb: { Display: 'DogNZB', Enabled: true },
